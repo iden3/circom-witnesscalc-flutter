90d7ee6a48fa0ac9c4aacdbbd2e256979a41a0b8
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-11-25 13:58:49 +0100

    Added integration tests.

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -8,23 +8,58 @@
 
 import 'dart:typed_data';
 
+import 'package:flutter/services.dart';
+import 'package:flutter_rapidsnark/flutter_rapidsnark.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:circom_witnesscalc/circom_witnesscalc.dart';
 
+const zkeyPath = 'assets/authV2.zkey';
+const inputsPath = 'assets/authV2_inputs.json';
+const wcdPath = 'assets/authV2.wcd';
+const vkPath = 'assets/authV2_verification_key.json';
+
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  final CircomWitnesscalc plugin = CircomWitnesscalc();
+  final plugin = CircomWitnesscalc();
+  final rapidsnark = Rapidsnark();
 
-  // TODO Test this
-  testWidgets('authV2 test', (WidgetTester tester) async {
+  Uint8List zkey = Uint8List(0);
+  String inputs = '';
+  Uint8List witnessGraph = Uint8List(0);
+  String verificationKey = '';
+
+  setUpAll(() async {
+    zkey = (await rootBundle.load(zkeyPath)).buffer.asUint8List();
+    inputs = await rootBundle.loadString(inputsPath);
+    witnessGraph = (await rootBundle.load(wcdPath)).buffer.asUint8List();
+    verificationKey = await rootBundle.loadString(vkPath);
+  });
+
+  testWidgets('Test witness calculation', (tester) async {
     final Uint8List? witness = await plugin.calculateWitness(
-      inputs: "",
-      graphData: Uint8List(0),
+      inputs: inputs,
+      graphData: witnessGraph,
     );
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
+
+    expect(witness, isNotNull);
     expect(witness?.isNotEmpty, true);
+
+    final proof = await rapidsnark.groth16Prove(
+      zkey: zkey,
+      witness: witness!,
+    );
+
+    expect(proof.proof, isNotEmpty);
+    expect(proof.publicSignals, isNotEmpty);
+
+    final verify = await rapidsnark.groth16Verify(
+      proof: proof.proof,
+      inputs: proof.publicSignals,
+      verificationKey: verificationKey,
+    );
+
+    expect(verify, isTrue);
   });
 }
